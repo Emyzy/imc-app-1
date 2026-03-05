738371909ca13d83b2ecb76b2078301f54677343
Emylly Vitória Pereira de Sousa Silva <241.14.039@EDU.gruposete.local>
2026-03-04 22:10:50 -0300

    Primeiro commit

---
+++ components/Topo.tsx
@@ -0,0 +1,22 @@
+import { Image, StyleSheet, View } from "react-native";
+
+export function Topo() {
+    return(
+    <View style={styles.containerLogo}>
+        <Image style={styles.logo} source={require('../assets/logo_app_imc.png')} />
+    </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    containerLogo: {
+        height: 150,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    logo: {
+        width: 170,
+        height: 60
+    }
+}
+)
